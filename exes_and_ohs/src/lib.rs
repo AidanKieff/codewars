9909d512d32c1c8f1fb9fe38b38db8fac79e63e9
Aidan K <ak@umorpha.io>
2024-01-16 10:13:51 -0700

    started exes and ohs

---
+++ exes_and_ohs/src/lib.rs
@@ -0,0 +1,16 @@
+
+fn xo(string: &'static str) -> bool {
+    false
+  }
+
+#[cfg(tests)]
+
+#[test]
+fn returns_expected() {
+    assert_eq!(xo("xo"), true);
+    assert_eq!(xo("Xo"), true);
+    assert_eq!(xo("xxOo"), true);
+    assert_eq!(xo("xxxm"), false);
+    assert_eq!(xo("Oo"), false);
+    assert_eq!(xo("ooom"), false);
+  }
