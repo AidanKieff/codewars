93f8431726ce609ba339db4d211720e9c315fa26
Aidan K <ak@umorpha.io>
2024-01-16 10:44:14 -0700

    added community found solution

--- exes_and_ohs/src/lib.rs
+++ exes_and_ohs/src/lib.rs
@@ -32,6 +32,17 @@ fn xo(string: &'static str) -> bool {
 }
 
 
+// community better fn -------------
+// fn xo(string: &'static str) -> bool {
+//     string.chars().fold(0, |a, c|{
+//       match c {
+//         'x' | 'X' => a + 1,
+//         'o' | 'O' => a - 1,
+//         _ => a
+//       }
+//     }) == 0
+//   }
+
 
 #[cfg(test)]
 
